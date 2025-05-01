3a0b7aabce010f47fcdb2725db249789594c31fe
fahim2k25 <fahim.program@gmail.com>
2025-05-01 21:14:56 +0600

    added tailwind css and daisyUI to the project

--- src/Title.jsx
+++ src/Title.jsx
@@ -2,6 +2,7 @@
 export default function Title() {
     return (
         <div>
+            <img src="../public/naohe.jpg" alt="exccel cake image" />
             <h1>Vanilla Fudge</h1>
             <p>Yes Hell on Earth</p>
         </div>
