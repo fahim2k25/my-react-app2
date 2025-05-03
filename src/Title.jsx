0a80f9027694aff9f991e7733f43e5c2f492f5e2
fahim2k25 <fahim.program@gmail.com>
2025-05-03 16:11:55 +0600

    implemented a addition button using Reat Hook add

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,5 +1,16 @@
+import {
+    useState
 
+} from "react";
 export default function Title() {
+
+    const [order, setOrder] = useState(0);
+
+    const handleOrderIncrease = () => {
+        const count = order + 1;
+        return setOrder(count);
+    }
+
     return (
         <div className="text-center">
             <img className="w-96 h-96 border-4 border-fuchsia-400 rounded-2xl" src="/naohe.jpg" alt="excellent cake image" />
@@ -7,8 +18,8 @@ export default function Title() {
             <p>Yes Sweetness Overloaded!!</p>
             <section className="border-2 border-lime-400 rounded-xl">
 
-                <h5 className="font-black text-2xl">Order Number: 0</h5>
-                <button className="m-3 p-2 bg-fuchsia-700 text-lg text-black font-semibold rounded-3xl hover:bg-amber-200 hover:cursor-pointer">Add</button>
+                <h5 className="font-black text-2xl">Order Number: {order}</h5>
+                <button onClick={handleOrderIncrease} className="m-3 p-2 bg-fuchsia-700 text-lg text-black font-semibold rounded-3xl hover:bg-amber-200 hover:cursor-pointer">Add</button>
                 <button className="m-3 p-2 bg-fuchsia-700 text-lg text-black font-semibold rounded-3xl hover:bg-amber-200 hover:cursor-pointer">Subtract</button>
                 <button className="m-3 p-2 bg-fuchsia-700 text-lg text-black font-semibold rounded-3xl hover:bg-amber-200 hover:cursor-pointer">Reset</button>
             </section>
